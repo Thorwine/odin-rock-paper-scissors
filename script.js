b38f6825288696d5d0863de62041d5cb99fa6942
Thorwine <thorwine69@gmail.com>
2022-07-14 13:11:41 +0200

    Create function playRound

--- script.js
+++ script.js
@@ -1,45 +1,77 @@
 // HTML-DOM > click on paragraph 'Play Now!'
-document.getElementById('playNow').addEventListener('click', testFunction)
+document.getElementById('playNow').addEventListener('click', test)
 
-function testFunction() {
-  let testVariable = playerPlay();
-  console.log(testVariable);
+function test() {
+  const playerSelection = playerPlay();
+  const computerSelection = computerPlay();
+
+  console.log(playRound(playerSelection, computerSelection));
 }
 
 // ------------------------------------------------------------------------
+function playRound(playerSelection, computerSelection) {
+  // Returns the result of a single round
 
-// const playerSelection = 'Rock';
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
+  switch (playerSelection) {
+    // Player 'Rock'
+    case 'Rock':
+      switch (computerSelection) {
+        case 'Rock':
+          return ('Draw! Rock vs. Rock');
+        case 'Paper':
+          return ('You lose! Paper beats Rock')
+        case 'Scissors':
+          return ('You win! Rock beats Scissors')
+      }
+    // Player 'Paper'
+    case 'Paper':
+      switch (computerSelection) {
+        case 'Rock':
+          return ('You win! Paper beats Rock');
+        case 'Paper':
+          return ('Draw! Paper vs. Paper');
+        case 'Scissors':
+          return ('You lose! Scissors beats Paper');
+      }
+    // Player 'Scissors'
+    case 'Scissors':
+      switch (computerSelection) {
+        case 'Rock':
+          return ('You lose! Rock beats Scissors');
+        case 'Paper':
+          return ('You win! Scissors beats Paper');
+        case 'Scissors':
+          return ('Draw! Scissors vs. Scissors');
+      }
+  }
+}
 
 // ------------------------------------------------------------------------
-
-// Randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay() {
+  // Returns either ‘Rock’, ‘Paper’ or ‘Scissors’ (randomly)
 
   let randomNumber = Math.floor(Math.random() * 3 + 1);
 
   switch (randomNumber) {
     case 1:
-      // console.log('Rock');
       return ('Rock');
       break;
     case 2:
-      // console.log('Paper');
       return ('Paper');
       break;
     case 3:
-      // console.log('Scissors');
       return ('Scissors');
       break;
   }
 }
 
-// Returns either ‘Rock’, ‘Paper’ or ‘Scissors’ by players choice
+// ------------------------------------------------------------------------
 function playerPlay() {
+  // Returns either ‘Rock’, ‘Paper’ or ‘Scissors’ (by players choice)
+
   let playerInput = prompt('Make your choice! Type \'Rock\', \'Paper\' or \'Scissors\'...');
 
-  // Check if prompt was not empty
+  // Check if prompt is not empty
   if (!(playerInput === null)) {
     // Convert player input to lower case
     playerInput = playerInput.toLowerCase();
@@ -48,8 +80,8 @@ function playerPlay() {
     // Combine first letter with player input (minus the first letter)
     playerInput = firstLetterBig + playerInput.slice(1);
   } else {
-    // Promt was empty, exit game
-    console.warn('Prompt was canceled')
+    // Promt is empty, exit game!
+    console.warn('Prompt empty > cancel!')
     return;
   }
 
