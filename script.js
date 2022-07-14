28849c27f83b0b0736d63c5a1ba7bedda8a89fec
Thorwine <thorwine69@gmail.com>
2022-07-14 08:13:56 +0200

    Create function computerPlay, add Play Now! link to HTML for script testing

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Test');
+// HTML-DOM > Click on paragraph 'Play Now!'
+document.getElementById('playNow').addEventListener('click', computerPlay)
+
+// Randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’
+function computerPlay() {
+
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  switch (randomNumber) {
+    case 1:
+      console.log('Rock');
+      return ('Rock');
+      break;
+    case 2:
+      console.log('Paper');
+      return ('Paper');
+      break;
+    case 3:
+      console.log('Scissors');
+      return ('Scissors');
+      break;
+  }
+}
